88b193e4668ea4b2b13c33d79011490c9a927cb7
hayden365 <ysmh100@gmail.com>
2023-07-21 23:29:02 +0900

    mealdetail

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,18 +1,34 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native';
-import { MEALS } from '../data/dummy-data';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 import MealItem from '../components/MealItem';
+import { useLayoutEffect } from 'react';
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route, navigation }) {
 	const catId = route.params.categoryId;
 
 	const displayMeals = MEALS.filter(mealItem => {
 		return mealItem.categoryIds.indexOf(catId) >= 0;
 	});
 
+	const categoryTitle = CATEGORIES.find(
+		category => category.id === catId,
+	).title;
+
+	useLayoutEffect(() => {
+		const categoryTitle = CATEGORIES.find(
+			category => category.id === catId,
+		).title;
+
+		navigation.setOptions({
+			title: categoryTitle,
+		});
+	}, [catId, navigation]);
+
 	function renderMealItem(itemData) {
 		const item = itemData.item;
 
 		const mealItemProps = {
+			id: item.id,
 			title: item.title,
 			imageUrl: item.imageUrl,
 			affordability: item.affordability,
